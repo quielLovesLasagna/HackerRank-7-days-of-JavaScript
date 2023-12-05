8f87863671e5b40876e3eb6b2eaa397d6be2ccc3
Exequiel Arco <137391972+quielLovesLasagna@users.noreply.github.com>
2023-12-05 09:39:40 +0800

    Add files via upload

---
+++ Day 3 - Display Information from an Object/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const library = [
+	{
+		title: "Bill Gates",
+		author: "The Road Ahead",
+		readingStatus: true,
+	},
+	{
+		title: "Steve Jobs",
+		author: "Walter Isaacson",
+		readingStatus: true,
+	},
+	{
+		title: "Mockingjay: The Final Book of The Hunger Games",
+		author: "Suzanne Collins",
+		readingStatus: false,
+	},
+];
+
+function displayInformation() {
+	library.forEach((book) => {
+		if (book.readingStatus) {
+			console.log(`Already read '${book.title}' by ${book.author}.`);
+		} else {
+			console.log(`You still need to read '${book.title}' by ${book.author}.`);
+		}
+	});
+}
+
+displayInformation();
